48115cd785f88fb5d931a03902d61db323e48097
chiemenaokafor <chiemenaokafor.co@gmail.com>
2021-11-08 09:24:26 +0100

    current skills components

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,8 +2,22 @@ import React from "react";
 
 function Skills() {
   return (
-    <div>
-      <h2>Skills</h2>
+    <div className="skills">
+      <h2 className="skills-h2">Skills</h2>
+      <div className="skills-content">
+        <div className="skills-content-dev">
+          <h5>Web Development</h5>
+          <ul className="skills-content-ul">
+            <li className="skills-content-neu">Dev</li>
+          </ul>
+        </div>
+        <div className="skills-content-dev">
+          <h5>UI & UX Design</h5>
+          <ul className="skills-content-ul">
+            <li className="skills-content-neu">Dev</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
