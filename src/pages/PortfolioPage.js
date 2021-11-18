a1af1b305da0239b9013cab201ab2eeb03ebf68a
chiemenaokafor <chiemenaokafor.co@gmail.com>
2021-11-18 11:46:41 +0100

    new portfolio

--- src/pages/PortfolioPage.js
+++ src/pages/PortfolioPage.js
@@ -1,9 +1,137 @@
 import React from "react";
+import cardImg from "./img/card-img.png";
 
 function Portfolio() {
   return (
-    <div>
-      <h2>Not complete</h2>
+    <div className="portfolio-page">
+      <h2>Finished Projects</h2>
+      <div className="portfolio-page-container">
+        <div className="port-card">
+          <div className="port-card-head">
+            <img src={cardImg} alt="project image" />
+          </div>
+          <div className="port-card-body">
+            <h6 className="port-card-name">PropertyHunt.ng</h6>
+            <p className="port-card-text">
+              lorgonrg rgonrigbrubew rr;grgr rfbrgbrg rg;ser;r irbr fwf;rnrwbb
+              rg;birugbrgb rgg;fvbfg;rb r;grbo4nfew rwbwuiotwnb efb ofiw4r93w
+              woit94 5et4 444 490w wrtg;nff;sgnwibtw3 23n23t320hj94jwrg
+              ;wn4tw4hnt rgru erbr ghrgbrlgu erligbrgl;GB ERGBRUILGRUGB;A wg
+              wtwr wrrww rger rrgrwer rerger
+            </p>
+            <ul className="port-card-tools">
+              <li>node</li>
+              <li>express</li>
+              <li>react</li>
+              <li>mongodb</li>
+            </ul>
+            <ul className="port-card-links">
+              <li>@</li>
+              <li>&</li>
+            </ul>
+          </div>
+        </div>
+        <div className="port-card">
+          <div className="port-card-head">
+            <img src={cardImg} alt="project image" />
+          </div>
+          <div className="port-card-body">
+            <h6 className="port-card-name">PropertyHunt.ng</h6>
+            <p className="port-card-text">
+              lorgonrg rgonrigbrubew rr;grgr rfbrgbrg rg;ser;r irbr fwf;rnrwbb
+              rg;birugbrgb rgg;fvbfg;rb r;grbo4nfew rwbwuiotwnb efb ofiw4r93w
+              woit94 5et4 444 490w wrtg;nff;sgnwibtw3 23n23t320hj94jwrg
+              ;wn4tw4hnt rgru erbr ghrgbrlgu erligbrgl;GB ERGBRUILGRUGB;A wg
+              wtwr wrrww rger rrgrwer rerger
+            </p>
+            <ul className="port-card-tools">
+              <li>node</li>
+              <li>express</li>
+              <li>react</li>
+              <li>mongodb</li>
+            </ul>
+            <ul className="port-card-links">
+              <li>@</li>
+              <li>&</li>
+            </ul>
+          </div>
+        </div>
+        <div className="port-card">
+          <div className="port-card-head">
+            <img src={cardImg} alt="project image" />
+          </div>
+          <div className="port-card-body">
+            <h6 className="port-card-name">PropertyHunt.ng</h6>
+            <p className="port-card-text">
+              lorgonrg rgonrigbrubew rr;grgr rfbrgbrg rg;ser;r irbr fwf;rnrwbb
+              rg;birugbrgb rgg;fvbfg;rb r;grbo4nfew rwbwuiotwnb efb ofiw4r93w
+              woit94 5et4 444 490w wrtg;nff;sgnwibtw3 23n23t320hj94jwrg
+              ;wn4tw4hnt rgru erbr ghrgbrlgu erligbrgl;GB ERGBRUILGRUGB;A wg
+              wtwr wrrww rger rrgrwer rerger
+            </p>
+            <ul className="port-card-tools">
+              <li>node</li>
+              <li>express</li>
+              <li>react</li>
+              <li>mongodb</li>
+            </ul>
+            <ul className="port-card-links">
+              <li>@</li>
+              <li>&</li>
+            </ul>
+          </div>
+        </div>
+        <div className="port-card">
+          <div className="port-card-head">
+            <img className="port-card-img" src={cardImg} alt="project image" />
+          </div>
+          <div className="port-card-body">
+            <h6 className="port-card-name">PropertyHunt.ng</h6>
+            <p className="port-card-text">
+              lorgonrg rgonrigbrubew rr;grgr rfbrgbrg rg;ser;r irbr fwf;rnrwbb
+              rg;birugbrgb rgg;fvbfg;rb r;grbo4nfew rwbwuiotwnb efb ofiw4r93w
+              woit94 5et4 444 490w wrtg;nff;sgnwibtw3 23n23t320hj94jwrg
+              ;wn4tw4hnt rgru erbr ghrgbrlgu erligbrgl;GB ERGBRUILGRUGB;A wg
+              wtwr wrrww rger rrgrwer rerger
+            </p>
+            <ul className="port-card-tools">
+              <li>node</li>
+              <li>express</li>
+              <li>react</li>
+              <li>mongodb</li>
+            </ul>
+            <ul className="port-card-links">
+              <li>@</li>
+              <li>&</li>
+            </ul>
+          </div>
+        </div>
+        <div className="port-card">
+          <div className="port-card-head">
+            <img src={cardImg} alt="project image" />
+          </div>
+          <div className="port-card-body">
+            <h6 className="port-card-name">PropertyHunt.ng</h6>
+            <p className="port-card-text">
+              lorgonrg rgonrigbrubew rr;grgr rfbrgbrg rg;ser;r irbr fwf;rnrwbb
+              rg;birugbrgb rgg;fvbfg;rb r;grbo4nfew rwbwuiotwnb efb ofiw4r93w
+              woit94 5et4 444 490w wrtg;nff;sgnwibtw3 23n23t320hj94jwrg
+              ;wn4tw4hnt rgru erbr ghrgbrlgu erligbrgl;GB ERGBRUILGRUGB;A wg
+              wtwr wrrww rger rrgrwer rerger
+            </p>
+            <ul className="port-card-tools">
+              <li>node</li>
+              <li>express</li>
+              <li>react</li>
+              <li>mongodb</li>
+            </ul>
+            <ul className="port-card-links">
+              <li>@</li>
+              <li>&</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
